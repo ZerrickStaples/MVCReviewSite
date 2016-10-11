c7e3775e029d5b2f033154d6550119f56f2f42d6
zerrickstaples <zerrick.staples@gmail.com>
2016-10-11 09:30:28 -0400

    try again

---
+++ MVCReviewSite/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSite.Models
+    /*Created Category model*/
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        [Display(Name = "Category")]
+        public string Name { get; set; }
+        public virtual ICollection <Review> Review { get; set; }
+    }
+}
---
+++ MVCReviewSite/Models/Review.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSite.Models
+{
+    /*Created Review model*/
+    public class Review
+    {
+        /*Create properties for review*/
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Review")]
+        public string Content { get; set; }
+        [Display(Name = "Publish Date")]
+        public DateTime PublishDate { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID {get; set;}
+        public virtual Category Category { get; set; }
+
+    }
+}
